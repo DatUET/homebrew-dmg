19cfd6345d1ec070d3966a1acd0f54cef99a0630
DatUET <17020677@vnu.edu.vn>
2025-02-27 21:10:19 +0700

    Update mobile-generate.rb

--- mobile-generate.rb
+++ mobile-generate.rb
@@ -1,16 +1,32 @@
 class MobileGenerate < Formula
   desc "This is tool for generate mobile for Darwin"
   homepage ""
-  url "https://github.com/DatUET/mobile-generate/archive/refs/tags/v0.0.4.tar.gz"
-  sha256 "6888ead8f8604740466073b6cc48fc39f6e0c5f8ac6f52a6f87f8f3d176bb8c7"
+  url "https://github.com/DatUET/mobile-generate/archive/refs/tags/v0.0.8.tar.gz"
+  # url "file:///Users/datpham/Desktop/test/mobile-generate/dmg.tar.gz"
+  sha256 "f2c72741c9e0d0a3c6754c1026419bbc148fb42a230ad730b1d83d28d113de1e"
   license ""
+  version "0.0.8"
+  depends_on "go" => :build
+  depends_on "unzip"
+  depends_on "bash"
+  
+  def fg_color(string, rgb_values)
+    "\e[38;2;#{rgb_values}m#{string}\e[0m"
+  end
 
   def install
-    bin.install "dmg.sh" => "dmg"
+    puts fg_color("Enter install pasword: ", "139;233;253")
+    system ("bash dmg_install.sh")
+    bin.install Dir["bin/*"]
+    system ("go version")
+    system ("go mod init mobile_generate") if !File.exist?("go.mod") # Khởi tạo module Go
+    system ("go mod tidy") # Cài đặt dependencies
+    system ("go build -o dmg main.go") # Build binary
+    bin.install "dmg" => "dmg"
   end
 
   test do
-    expected_version = "0.0.4"
+    expected_version = "0.0.5"
     actual_version = shell_output("#{bin}/dmg --version").strip
     assert_match expected_version, actual_version
   end
