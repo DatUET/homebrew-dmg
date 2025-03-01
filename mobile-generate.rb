3629ac40d2a26ab8a654e0fab1910e95cc37b960
DatUET <17020677@vnu.edu.vn>
2025-03-01 21:50:55 +0700

    Update mobile-generate.rb

--- mobile-generate.rb
+++ mobile-generate.rb
@@ -1,32 +1,30 @@
 class MobileGenerate < Formula
   desc "This is tool for generate mobile for Darwin"
   homepage ""
-  url "https://github.com/DatUET/mobile-generate/archive/refs/tags/v0.0.10.tar.gz"
-  # url "file:///Users/datpham/Desktop/test/mobile-generate/dmg.tar.gz"
-  sha256 "005faf8b2068869b4d3bc1cfeed5eece277578aa99103d29d57cdb0752499fcd"
+  url "https://github.com/DatUET/mobile-generate/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "59838ecad2e2e81d490180be1c328640f3046e413426e71d40c25e72ea44e6fd"
   license ""
-  version "0.0.10"
+  version "0.1.0"
   depends_on "go" => :build
-  depends_on "unzip"
-  depends_on "bash"
+  depends_on "git"
+
   
   def fg_color(string, rgb_values)
     "\e[38;2;#{rgb_values}m#{string}\e[0m"
   end
 
   def install
-    puts fg_color("Enter install pasword: ", "139;233;253")
-    system ("bash dmg_install.sh")
+    system ("git clone ssh://git@gitlab.com/DatUET/mobile_generate_script.git bin")
     bin.install Dir["bin/*"]
     system ("go version")
-    system ("go mod init mobile_generate") if !File.exist?("go.mod") # Khởi tạo module Go
-    system ("go mod tidy") # Cài đặt dependencies
-    system ("go build -o dmg main.go") # Build binary
+    system ("go mod init mobile_generate") if !File.exist?("go.mod")
+    system ("go mod tidy")
+    system ("go build -o dmg main.go")
     bin.install "dmg" => "dmg"
   end
 
   test do
-    expected_version = "0.0.5"
+    expected_version = "0.1.0"
     actual_version = shell_output("#{bin}/dmg --version").strip
     assert_match expected_version, actual_version
   end
