ded9c1548c1891bdf057e7708b48cdfacb47442a
DatUET <17020677@vnu.edu.vn>
2025-03-19 14:37:58 +0700

    Update mobile-generate.rb

--- mobile-generate.rb
+++ mobile-generate.rb
@@ -1,9 +1,9 @@
 class MobileGenerate < Formula
   desc "This is tool for generate mobile for Darwin"
   homepage ""
-  url "https://github.com/DatUET/mobile-generate/archive/refs/tags/v0.1.4.tar.gz"
+  url "https://github.com/DatUET/mobile-generate/archive/refs/tags/v0.1.5.tar.gz"
 #   url "file:///Users/datpham/Desktop/test/dmg/mobile-generate/dmg.tar.gz"
-  sha256 "782e7d9d40a5c05eb875ba7051caa805b7632cb5e0c52f56a8e671728ec0649c"
+  sha256 "8e42b2affb75228e1596ab7af7f17f6d6e0450c5207ff9c0ed94a719d4d94807"
   license ""
   version "0.1.5"
   depends_on "go" => :build
